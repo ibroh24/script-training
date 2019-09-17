14956082aafca182a9edace7a893bea0ed32d7d2
Ibrahim Hammed <ibroh24@gmail.com>
2019-09-17 22:55:59 +0000

    exploring dom

--- Script/domjavascript.js
+++ Script/domjavascript.js
@@ -2,7 +2,7 @@ const heading = document.getElementById('myHeading');
 const mytext = document.getElementById('mytext');
 const button = document.getElementById('myButton');
 const btn2 = document.getElementById('myButton2');
-
+const myList = document.getElementsByTagName('li');
 button.addEventListener('click', ()=>{
     heading.style.color = mytext.value;
     heading.style.backgroundColor = 'green';
@@ -13,4 +13,11 @@ btn2.addEventListener('click', () => {
     heading.style.color = 'black';
     heading.style.backgroundColor = 'white';
     
+})
+
+button.addEventListener('click', () => {
+    for (let i = 0; i < myList.length; i++) {
+        myList[i].style.color = mytext.value;
+        
+    }
 })
