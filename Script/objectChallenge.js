cabf8fc180aa4359c3236c3ecc64d9049ca55bd8
Ibrahim Hammed <ibroh24@gmail.com>
2019-09-17 16:12:15 +0000

    object challenge

--- Script/objectChallenge.js
+++ Script/objectChallenge.js
@@ -24,9 +24,48 @@ var ourStudent = [
         point: 250
     },
     {
-        name: '',
-        track: ['C#', 'JavaScript', 'HTML', 'CSS'],
+        name: 'Ibrahim Hammed',
+        track: ['C#', 'JavaScript', 'PHP', 'NodeJs'],
         achievement: 4,
         point: 100
     }
 ]
+var message
+console.log(ourStudent);
+// for(var i = 0; i<ourStudent.length; i++){
+//     var message = '<p><b>Student: '+ourStudent[i].name+ '</b></p>';
+//     message += '<p>Track: '+ourStudent[i].track.join(', ')+ '</p>';
+//     message += '<p>Achievement: '+ourStudent[i].achievement+ '</p>';
+//     message += '<p>Points: '+ourStudent[i].point+ '</p>';
+//     message += '<br>';
+//     document.write(JSON.stringify(message));
+// }
+
+var userInput = prompt("Search for any Student Record in our System, Type Quit to exit or type all to view all records");
+userInput.toLowerCase();
+do {
+    if(userInput ==='quit'){
+        break;
+    }else if(userInput ==='all'){
+        for(var i = 0; i<ourStudent.length; i++){
+            message = '<p><b>Student: '+ourStudent[i].name+ '</b></p>';
+            message += '<p>Track: '+ourStudent[i].track.join(', ')+ '</p>';
+            message += '<p>Achievement: '+ourStudent[i].achievement+ '</p>';
+            message += '<p>Points: '+ourStudent[i].point+ '</p>';
+            message += '<br>';
+            document.write(JSON.stringify(message));
+        }
+        break;
+    }else{
+        if(userInput === ourStudent[i].name){
+            message = '<p><b>Student: '+ourStudent[i].name+ '</b></p>';
+            message += '<p>Track: '+ourStudent[i].track.join(', ')+ '</p>';
+            message += '<p>Achievement: '+ourStudent[i].achievement+ '</p>';
+            message += '<p>Points: '+ourStudent[i].point+ '</p>';
+            message += '<br>';
+            document.write(JSON.stringify(message));
+            break;
+        }
+    }
+    
+} while (true);
