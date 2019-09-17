d4494a6dc6794add93098c2651170778a1e4515e
Ibrahim Hammed <ibroh24@gmail.com>
2019-09-17 14:59:17 +0000

    object challenge

---
+++ Script/objectChallenge.js
@@ -0,0 +1,32 @@
+var ourStudent = [
+    {
+        name: 'Uthman Majadtek',
+        track: ['FrontEnd development', 'BackEnd Development'],
+        achievement: 5,
+        point: 200
+    },
+    {
+        name: 'Abdillah Sulayman',
+        track: ['C#', 'JavaScript', 'HTML', 'CSS'],
+        achievement: 4,
+        point: 100
+    },
+    {
+        name: 'Omar Sulayman',
+        track: ['Web Development', 'JavaScript', '.Net Frameworks'],
+        achievement: 5,
+        point: 150
+    },
+    {
+        name: 'Dr. Yasir Quadri',
+        track: ['Adventure of Islam', 'Prophetic Medicine', 'The Seal Nectar'],
+        achievement: 10,
+        point: 250
+    },
+    {
+        name: '',
+        track: ['C#', 'JavaScript', 'HTML', 'CSS'],
+        achievement: 4,
+        point: 100
+    }
+]
