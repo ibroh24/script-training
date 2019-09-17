633cf1bdf28bd989bfd91755bdd06dfd849e5861
Ibrahim Hammed <ibroh24@gmail.com>
2019-09-17 14:35:10 +0000

    object training

--- Script/javascriptObjects.js
+++ Script/javascriptObjects.js
@@ -5,8 +5,17 @@ var person = {
     stack: ['PHP', 'C#.Net', 'Javascript']
 }
 
-function getPrint(message) {
+function getPrint(note) {
     var check = document.getElementById('output');
-    return check.innerHTML = message;
+    check.innerHTML = note;
 }
 
+var message = '<p>My name is '+person.name+ ' and am just '+person.age+ 'years old </p>';
+person.country = 'Nigeria';
+message += '<p>I lived in '+person.country+ ' and am a '+person.job+'</p>'
+message += '<p>My stack are '+person.stack.length+' Namely: '+person.stack.join(', ')+'</p>'
+getPrint(message);
+
+for (var key in person) {
+    console.log(key, ':', person[key]);
+}
